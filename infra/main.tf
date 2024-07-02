d2f3c068547875e36ab5e8f38d10f3ed515cc529
Pedro Alejandro Pacheco Tripi <pedro-alejandro.pacheco-tripi@dormakaba.com>
2024-07-02 10:49:14 +0200

    connection to Azure via Service Principal

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  subscription_id = var.ARM_SUPSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
 }
 
 module "boilerplate" {
